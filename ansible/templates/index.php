12d3f561b6c6d6eee843fd9935a2857b1bc4a571
Claudia Perez <>
2017-11-23 03:14:08 +0100

    ajuste a la configuración del ambiente del ambiente de producción

--- ansible/templates/index.php
+++ ansible/templates/index.php
@@ -1,2 +1,9 @@
-<?php
- echo "Hello World. Sistema operativo" . {{ ansible_distribution }} . "  " . {{ ansible_distribution_major_version}};
+
+<html>
+
+<body>
+ <h1>
+ 	Hello World. Sistema operativo  
+ 	{{ ansible_distribution }} 	{{ ansible_distribution_major_version}};
+ </h1>
+</body>
